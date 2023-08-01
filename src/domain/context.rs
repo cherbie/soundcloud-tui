a43b8dc171bc2764cb3d8e75a32e42d1dc835475
Clayton <48974591+cherbie@users.noreply.github.com>
2023-08-01 17:59:36 +1000

    refactor: distinguish between ui, domain and event logic (#10)

--- src/domain/context.rs
+++ src/domain/context.rs
@@ -5,28 +5,28 @@ use std::sync::Mutex;
 use tui::backend::{Backend, CrosstermBackend};
 use tui::terminal::Terminal;
 
-use self::route::Route;
+use super::router::Router;
 use crate::event;
 
-pub struct App<B>
+pub struct Context<B>
 where
     B: Backend,
 {
     terminal: RefCell<Terminal<B>>,
-    route: Route,
+    router: Router,
     pub events: Mutex<event::Events>,
 }
 
-impl App<CrosstermBackend<io::Stdout>> {
+impl Context<CrosstermBackend<io::Stdout>> {
     pub fn new() -> Result<Self> {
         let stdout = io::stdout();
         let backend = CrosstermBackend::new(stdout);
         let mut terminal = Terminal::new(backend)?;
         terminal.hide_cursor()?;
 
-        Ok(App {
+        Ok(Context {
             terminal: RefCell::new(terminal),
-            route: Route::default(),
+            router: Router::default(),
             events: Mutex::new(event::Events::default()),
         })
     }
