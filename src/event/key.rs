3f60c1f64683e49e548de725c9d311064e6c9721
Clayton Herbst <herbsca1@gmail.com>
2024-05-02 07:51:19 +1000

    chore: fix clippy errors with minor version bumps

--- src/event/key.rs
+++ src/event/key.rs
@@ -45,11 +45,13 @@ impl From<event::KeyEvent> for Key {
             event::KeyEvent {
                 code: event::KeyCode::Char('c'),
                 modifiers: event::KeyModifiers::ALT,
+                ..
             } => Key::Alt('c'),
 
             event::KeyEvent {
                 code: event::KeyCode::Char('c'),
                 modifiers: event::KeyModifiers::CONTROL,
+                ..
             } => Key::Ctrl('c'),
 
             event::KeyEvent {
