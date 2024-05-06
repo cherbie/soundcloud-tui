35ed2a30d9af90e428e54e21102be1e51b570366
Clayton Herbst <herbsca1@gmail.com>
2024-05-06 12:47:39 +1000

    build(ffplay): add library dependencies

--- application/ffplay/src/main.rs
+++ application/ffplay/src/main.rs
@@ -4,18 +4,17 @@ mod cli_test;
 
 use anyhow::Result;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let cli_matches = cli::args_parser().get_matches();
 
     // Gets a value for config if supplied by user, or defaults to "default.conf"
     let uri = cli_matches.get_one::<String>("input").unwrap();
-    println!("Using input uri: {}", uri);
 
-    // TODO: testing
-    match cli_matches.get_one::<u8>("verbose") {
-        Some(l) => println!("No verbose info: {}", l),
-        _ => print!("Some other verbosity"),
-    };
+    app(uri).await
+}
 
+async fn app(uri: &str) -> Result<()> {
+    print!("App started with uri: {}", uri);
     Ok(())
 }
